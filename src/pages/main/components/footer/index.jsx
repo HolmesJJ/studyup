d5a77ad841fb1d494d1aea42bc3df772067f1fcd
HU JIAJUN <hujiajun19961024@gmail.com>
2024-06-03 16:04:19 +0800

    Fix bugs

--- src/pages/main/components/footer/index.jsx
+++ src/pages/main/components/footer/index.jsx
@@ -8,9 +8,9 @@ const Footer = () => {
         Copyright © 2023 - 2024 studyup.io All Rights
         Reserved. 学上科技有限公司 版权所有
       </div>
-      <div className="footer_text">
+      {/* <div className="footer_text">
         京ICP证XXXXX号 京ICP证YYYYYY号 出版物经营许可证 京公网安备 ZZZZZZ号
-      </div>
+      </div> */}
     </div>
   );
 };
